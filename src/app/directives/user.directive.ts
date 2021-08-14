ca3c885888db8545dad27e5738aadafc5ba39527
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-14 14:27:31 +0300

    updated directive

--- src/app/directives/user.directive.ts
+++ src/app/directives/user.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appUser]'
 })
 export class UserDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) {
+    this.elem.nativeElement.style.backgroundColor = "rgb(249, 249, 255)"
+   }
 
 }
