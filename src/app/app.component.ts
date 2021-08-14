6ef05398796f8b9c421c7c7373e5bd64402c7107
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-14 14:08:06 +0300

    Added the find user git code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from './service/data.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GitSearch';
+  gitUser:any
+  gitHubRepos:any 
+  userName!:string 
+
+  constructor(private dataService:DataService){}
+  findUser(user:User){
+    this.userName = user.userName
+    console.log(this.userName)
+
+    this.dataService.getGitHubUsers(this.userName).subscribe(data => {this.gitUser = data
+      console.log(data)})
+}
+
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
+  apiKey:string = environment.gitApiKey
 
   constructor(private http:HttpClient) { }
 
